4bb9af8ca92fbac22c6708f3e5d0c2d3a26a4fb7
tanushaveraksa <veraksa161@gmail.com>
2022-05-24 05:32:00 +0300

    feat: create 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fs = require('fs');
+const path = require('path');
 
+let srcDir = path.join('04-copy-directory', 'files');
+
+fs.mkdir(path.join('04-copy-directory', 'files-copy'), { recursive: true }, (err) => {
+  if (err) throw err;
+  fs.readdir(srcDir,  'utf-8', (err, files) => {   
+    if (err) throw err;
+    files.forEach((elem) => {
+      fs.copyFile(path.join('04-copy-directory', 'files', elem), path.join('04-copy-directory', 'files-copy', elem), err => {
+        if(err) throw err; 
+        console.log('File copied successfully');
+      });
+    });
+  });
+}); 
+
+
+
+  
