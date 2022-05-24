c6154deb423bf3613866a876abc1e611dbba04a8
tanushaveraksa <veraksa161@gmail.com>
2022-05-24 09:31:15 +0300

    feat: ceate 05-merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+let srcDir = path.join('05-merge-styles', 'styles');
+let prjDir = path.join('05-merge-styles', 'project-dist');
+
+fs.unlink(path.format({dir: prjDir, base: 'bundle.css'}), err => {
+  if(err) throw err; 
+  console.log('File deleted');
+});
+
+fs.readdir(srcDir,  'utf-8', (err, files) => {   
+  if (err) throw err;
+  let filesCss = [...files].filter((elem)=> path.extname(elem) == '.css');
+
+  filesCss.forEach((elem) => {
+    const stream = fs.createReadStream(path.join('05-merge-styles', 'styles', elem));
+    stream.on('data', (data) => {
+      fs.appendFile('05-merge-styles/project-dist/bundle.css', '\n' + data, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log('Done');
+      });
+    });
+  });
+});
+
+
+//path.format({dir: prjDir, base: 'bundle.css'})
